6bdb86eaf289250f8d0d0dff668ab8bc63ec85e4
SurKaa <98200894+surkaa@users.noreply.github.com>
2024-08-18 21:51:20 +0800

    添加primevue

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import App from './App.vue';
 import './assets/style.css';
 import './assets/index.css';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import 'primeicons/primeicons.css'
 import router from "./router";
 import {createPinia} from "pinia";
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const pinia = createPinia();
 
 createApp(App)
     .use(pinia)
     .use(router)
-    .use(ElementPlus)
+    .use(ElementPlus).use(PrimeVue, {theme: {preset: Aura}})
     .mount('#app');
