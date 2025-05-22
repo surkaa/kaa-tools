7265aaf7837db3804d1e7455625b3d60bb647486
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-05-22 16:38:54 +0800

    优化 PDF 文件加载和渲染逻辑，支持多页显示并添加错误处理

--- src/views/CutPdf.vue
+++ src/views/CutPdf.vue
@@ -1,29 +1,58 @@
 <script setup lang="ts">
 import * as pdfLib from "pdfjs-dist";
-import {ref} from "vue";
+import {PDFDocumentProxy} from "pdfjs-dist";
+import {nextTick, ref} from "vue";
+import {ElMessage} from "element-plus";
 
-pdfLib.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.mjs'
+pdfLib.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.mjs';
 
+const pdfPages = ref(0);
+const pdfScale = ref(2);
 const selected = ref<number[]>([]);
 
-const inputChange = async (e: any) => {
+const renderPage = (num: number, pdfDoc: PDFDocumentProxy) => {
+  pdfDoc.getPage(num).then((page) => {
+    const canvas = document.getElementById(`page-${num}`) as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d')!!;
+    const viewport = page.getViewport({scale: pdfScale.value});
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+    page.render({
+      canvasContext: ctx,
+      viewport
+    });
+    if (num < pdfPages.value) {
+      nextTick(() => renderPage(num + 1, pdfDoc));
+    }
+  })
+}
+
+const inputChange = (e: any) => {
   const file = e.target.files[0];
   if (!file) return;
 
   const url = URL.createObjectURL(file);
-  const loadingTask = pdfLib.getDocument(url);
-  const pdfDoc = await loadingTask.promise;
-  selected.value = [];
-  for (let i = 0; i < pdfDoc.numPages; i++) {
-    selected.value.push(i);
-  }
-  console.log(pdfDoc);
+  const loadingTask = pdfLib.getDocument({
+    url,
+    cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.16.105/cmaps/',
+    cMapPacked: true,
+  });
+  loadingTask.promise.then(pdfDoc => {
+    selected.value = [];
+    pdfPages.value = pdfDoc.numPages;
+    // 从第一页开始渲染
+    nextTick(() => renderPage(1, pdfDoc));
+  }).catch(_err => ElMessage.error('加载错误'));
 }
 </script>
 
 <template>
   <div id="cut-pdf">
     <input type="file" @change="inputChange" accept="application/pdf">
+    <div id="pdf-view">
+      <canvas v-for="page in pdfPages" :key="page" :id="`page-${page}`"/>
+      <div id="text-view"></div>
+    </div>
   </div>
 </template>
 
@@ -32,5 +61,11 @@ const inputChange = async (e: any) => {
   width: 100%;
   height: 100%;
   background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+
+  #pdf-view {
+    overflow: auto;
+    display: flex;
+    flex-wrap: wrap;
+  }
 }
 </style>
