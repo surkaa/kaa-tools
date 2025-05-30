0963d8911cda7842d324e2fe96efb579966f2d3c
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-05-30 15:18:21 +0800

    格式化

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      }
-    }
-  },
-  plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            }
+        }
+    },
+    plugins: [vue()],
 })
