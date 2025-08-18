64a82a439244ec626d9ec16b389b092f692de94d
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-08-18 20:37:28 +0800

    优化 useLocalStorageRef 初始化逻辑，确保初始值存储在本地存储中

--- src/utils/useLocalStorgeRef.ts
+++ src/utils/useLocalStorgeRef.ts
@@ -2,7 +2,13 @@ import {ref, watch} from "vue";
 
 export const useLocalStorageRef = <T>(key: string, initialValue: T) => {
     const storedValue = localStorage.getItem(key);
-    const initial = storedValue ? JSON.parse(storedValue) : initialValue;
+    let initial;
+    if (storedValue) {
+        initial = JSON.parse(storedValue);
+    } else {
+        initial = initialValue;
+        localStorage.setItem(key, JSON.stringify(initialValue));
+    }
 
     const r = ref<T>(initial);
 
